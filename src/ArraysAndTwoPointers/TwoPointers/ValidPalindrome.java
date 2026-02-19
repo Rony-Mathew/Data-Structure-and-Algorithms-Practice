9a53a10dd836b1bbe88024530b32696f294caba2
Rony <ronymathewareekkattu@gmail.com>
2026-02-19 16:26:26 +0530

    Valid Palindrome Solution

---
+++ src/ArraysAndTwoPointers/TwoPointers/ValidPalindrome.java
@@ -0,0 +1,61 @@
+package ArraysAndTwoPointers.TwoPointers;
+
+
+//https://leetcode.com/problems/valid-palindrome/description/
+//
+//A phrase is a palindrome if, after converting all uppercase letters into lowercase letters and removing all non-alphanumeric characters, it reads the same forward and backward. Alphanumeric characters include letters and numbers.
+//
+//Given a string s, return true if it is a palindrome, or false otherwise.
+//
+//
+//
+//        Example 1:
+//
+//Input: s = "A man, a plan, a canal: Panama"
+//Output: true
+//Explanation: "amanaplanacanalpanama" is a palindrome.
+//        Example 2:
+//
+//Input: s = "race a car"
+//Output: false
+//Explanation: "raceacar" is not a palindrome.
+//Example 3:
+//
+//Input: s = " "
+//Output: true
+//Explanation: s is an empty string "" after removing non-alphanumeric characters.
+//Since an empty string reads the same forward and backward, it is a palindrome.
+//
+//
+//        Constraints:
+//
+//        1 <= s.length <= 2 * 105
+//s consists only of printable ASCII characters.
+
+public class ValidPalindrome {
+    public static void main(String[] args) {
+        boolean result = isPalindrome("A man, a plan, a canal: Panama");
+        System.out.println(result);
+    }
+    public static boolean isPalindrome(String s) {
+        char[] ch = s.toCharArray();
+        int left = 0;
+        int right = ch.length - 1;
+
+        while (left < right) {
+            while (left < right && !Character.isLetterOrDigit(ch[left])) {
+                left++;
+            }
+            while (left < right && !Character.isLetterOrDigit(ch[right])) {
+                right--;
+            }
+            if (Character.toLowerCase(ch[left]) != Character.toLowerCase(ch[right])) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+
+}
